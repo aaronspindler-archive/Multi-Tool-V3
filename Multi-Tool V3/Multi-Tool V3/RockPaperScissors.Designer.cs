33ecebce899876542bac21ccff92473172b48309
xNovax <nova1104@live.com>
2014-06-27 02:08:30 -0400

    Resized Rock Paper Scissors

--- Multi-Tool V3/Multi-Tool V3/RockPaperScissors.Designer.cs
+++ Multi-Tool V3/Multi-Tool V3/RockPaperScissors.Designer.cs
@@ -62,32 +62,33 @@
             this.tableLayoutPanel.Controls.Add(this.paperButton, 3, 2);
             this.tableLayoutPanel.Controls.Add(this.scissorsButton, 6, 2);
             this.tableLayoutPanel.Controls.Add(this.robotsChoiceDisplay, 0, 4);
-            this.tableLayoutPanel.Controls.Add(this.winnerDisplay, 2, 6);
+            this.tableLayoutPanel.Controls.Add(this.winnerDisplay, 2, 5);
             this.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.tableLayoutPanel.Name = "tableLayoutPanel";
-            this.tableLayoutPanel.RowCount = 9;
+            this.tableLayoutPanel.RowCount = 7;
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 38F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
             this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
-            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
-            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
-            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
-            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
-            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
-            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
-            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
             this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel.Size = new System.Drawing.Size(348, 209);
+            this.tableLayoutPanel.Size = new System.Drawing.Size(696, 323);
             this.tableLayoutPanel.TabIndex = 0;
             // 
             // rockButton
             // 
             this.tableLayoutPanel.SetColumnSpan(this.rockButton, 2);
             this.rockButton.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.rockButton.Location = new System.Drawing.Point(3, 46);
+            this.rockButton.Location = new System.Drawing.Point(6, 91);
+            this.rockButton.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.rockButton.Name = "rockButton";
             this.tableLayoutPanel.SetRowSpan(this.rockButton, 2);
-            this.rockButton.Size = new System.Drawing.Size(80, 40);
+            this.rockButton.Size = new System.Drawing.Size(162, 82);
             this.rockButton.TabIndex = 1;
             this.rockButton.Text = "Rock";
             this.rockButton.UseVisualStyleBackColor = true;
@@ -99,9 +100,10 @@
             this.tableLayoutPanel.SetColumnSpan(this.usernameDisplay, 4);
             this.usernameDisplay.Dock = System.Windows.Forms.DockStyle.Fill;
             this.usernameDisplay.ForeColor = System.Drawing.Color.DarkRed;
-            this.usernameDisplay.Location = new System.Drawing.Point(175, 0);
+            this.usernameDisplay.Location = new System.Drawing.Point(354, 0);
+            this.usernameDisplay.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.usernameDisplay.Name = "usernameDisplay";
-            this.usernameDisplay.Size = new System.Drawing.Size(170, 20);
+            this.usernameDisplay.Size = new System.Drawing.Size(336, 38);
             this.usernameDisplay.TabIndex = 1;
             this.usernameDisplay.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
@@ -112,7 +114,8 @@
             this.fileToolStripMenuItem});
             this.menuStrip.Location = new System.Drawing.Point(0, 0);
             this.menuStrip.Name = "menuStrip";
-            this.menuStrip.Size = new System.Drawing.Size(172, 20);
+            this.menuStrip.Padding = new System.Windows.Forms.Padding(12, 4, 0, 4);
+            this.menuStrip.Size = new System.Drawing.Size(348, 38);
             this.menuStrip.TabIndex = 0;
             this.menuStrip.Text = "menuStrip1";
             // 
@@ -121,13 +124,13 @@
             this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.exitToolStripMenuItem});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 16);
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(64, 30);
             this.fileToolStripMenuItem.Text = "File";
             // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(127, 36);
             this.exitToolStripMenuItem.Text = "Exit";
             this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
@@ -136,10 +139,10 @@
             this.rockPaperScissorsHelpLabel.AutoSize = true;
             this.tableLayoutPanel.SetColumnSpan(this.rockPaperScissorsHelpLabel, 8);
             this.rockPaperScissorsHelpLabel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.rockPaperScissorsHelpLabel.Location = new System.Drawing.Point(2, 20);
-            this.rockPaperScissorsHelpLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.rockPaperScissorsHelpLabel.Location = new System.Drawing.Point(4, 38);
+            this.rockPaperScissorsHelpLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.rockPaperScissorsHelpLabel.Name = "rockPaperScissorsHelpLabel";
-            this.rockPaperScissorsHelpLabel.Size = new System.Drawing.Size(344, 23);
+            this.rockPaperScissorsHelpLabel.Size = new System.Drawing.Size(688, 47);
             this.rockPaperScissorsHelpLabel.TabIndex = 2;
             this.rockPaperScissorsHelpLabel.Text = "Click a button below to select what item you will play";
             this.rockPaperScissorsHelpLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -148,10 +151,11 @@
             // 
             this.tableLayoutPanel.SetColumnSpan(this.paperButton, 2);
             this.paperButton.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.paperButton.Location = new System.Drawing.Point(132, 46);
+            this.paperButton.Location = new System.Drawing.Point(267, 91);
+            this.paperButton.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.paperButton.Name = "paperButton";
             this.tableLayoutPanel.SetRowSpan(this.paperButton, 2);
-            this.paperButton.Size = new System.Drawing.Size(80, 40);
+            this.paperButton.Size = new System.Drawing.Size(162, 82);
             this.paperButton.TabIndex = 3;
             this.paperButton.Text = "Paper";
             this.paperButton.UseVisualStyleBackColor = true;
@@ -161,10 +165,11 @@
             // 
             this.tableLayoutPanel.SetColumnSpan(this.scissorsButton, 2);
             this.scissorsButton.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.scissorsButton.Location = new System.Drawing.Point(261, 46);
+            this.scissorsButton.Location = new System.Drawing.Point(528, 91);
+            this.scissorsButton.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.scissorsButton.Name = "scissorsButton";
             this.tableLayoutPanel.SetRowSpan(this.scissorsButton, 2);
-            this.scissorsButton.Size = new System.Drawing.Size(84, 40);
+            this.scissorsButton.Size = new System.Drawing.Size(162, 82);
             this.scissorsButton.TabIndex = 4;
             this.scissorsButton.Text = "Scissors";
             this.scissorsButton.UseVisualStyleBackColor = true;
@@ -175,9 +180,10 @@
             this.robotsChoiceDisplay.AutoSize = true;
             this.tableLayoutPanel.SetColumnSpan(this.robotsChoiceDisplay, 8);
             this.robotsChoiceDisplay.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.robotsChoiceDisplay.Location = new System.Drawing.Point(3, 89);
+            this.robotsChoiceDisplay.Location = new System.Drawing.Point(6, 179);
+            this.robotsChoiceDisplay.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.robotsChoiceDisplay.Name = "robotsChoiceDisplay";
-            this.robotsChoiceDisplay.Size = new System.Drawing.Size(342, 23);
+            this.robotsChoiceDisplay.Size = new System.Drawing.Size(684, 47);
             this.robotsChoiceDisplay.TabIndex = 1;
             this.robotsChoiceDisplay.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
@@ -186,21 +192,23 @@
             this.winnerDisplay.AutoSize = true;
             this.tableLayoutPanel.SetColumnSpan(this.winnerDisplay, 4);
             this.winnerDisplay.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.winnerDisplay.Location = new System.Drawing.Point(89, 135);
+            this.winnerDisplay.Location = new System.Drawing.Point(180, 226);
+            this.winnerDisplay.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.winnerDisplay.Name = "winnerDisplay";
             this.tableLayoutPanel.SetRowSpan(this.winnerDisplay, 2);
-            this.winnerDisplay.Size = new System.Drawing.Size(166, 46);
+            this.winnerDisplay.Size = new System.Drawing.Size(336, 97);
             this.winnerDisplay.TabIndex = 5;
             this.winnerDisplay.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // RockPaperScissors
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(348, 209);
+            this.ClientSize = new System.Drawing.Size(696, 323);
             this.Controls.Add(this.tableLayoutPanel);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip;
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "RockPaperScissors";
             this.Text = "Rock Paper Scissors";
             this.Load += new System.EventHandler(this.RockPaperScissors_Load);
