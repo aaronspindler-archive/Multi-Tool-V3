ff3aa38a7bcb611a0cc3f20103bf6fd023a2bce9
xNovax <aaron@xnovax.net>
2015-09-17 00:45:26 -0400

    Modified license in program

--- Multi-Tool V3/Multi-Tool V3/Forms/License.Designer.cs
+++ Multi-Tool V3/Multi-Tool V3/Forms/License.Designer.cs
@@ -36,19 +36,21 @@
             // 
             this.licenseTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
             this.licenseTextBox.Location = new System.Drawing.Point(0, 0);
+            this.licenseTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.licenseTextBox.Name = "licenseTextBox";
             this.licenseTextBox.ReadOnly = true;
-            this.licenseTextBox.Size = new System.Drawing.Size(732, 524);
+            this.licenseTextBox.Size = new System.Drawing.Size(366, 272);
             this.licenseTextBox.TabIndex = 0;
             this.licenseTextBox.Text = resources.GetString("licenseTextBox.Text");
             // 
             // License
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(732, 524);
+            this.ClientSize = new System.Drawing.Size(366, 272);
             this.Controls.Add(this.licenseTextBox);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "License";
             this.Text = "License";
             this.ResumeLayout(false);
